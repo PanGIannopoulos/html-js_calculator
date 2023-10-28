6239e0605a8562840f875c8abbd3e629f7d651ea
Luminous <Panabougian@gmail.com>
2023-10-28 23:52:48 +0300

    [create initial website]

---
+++ skriptos.js
@@ -0,0 +1,7 @@
+function add() {
+    var a = document.getElementById('a').value;
+    var b = document.getElementById('b').value;
+    var sum = Number(a) + Number(b);
+    alert("ThNe sum of the 2 numbers is" + sum);
+    alert("what is this shit");
+}
