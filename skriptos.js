9b986955f969ea7ace848222c5c3973de0007551
Luminous <Panabougian@gmail.com>
2023-10-28 23:55:35 +0300

    Cleared some javascript mess

--- skriptos.js
+++ skriptos.js
@@ -3,5 +3,4 @@ function add() {
     var b = document.getElementById('b').value;
     var sum = Number(a) + Number(b);
     alert("ThNe sum of the 2 numbers is" + sum);
-    alert("what is this shit");
 }
