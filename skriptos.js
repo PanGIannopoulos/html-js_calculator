cf64edef9f6445989f5a6a562a90cf0bb2f24418
Luminous <Panabougian@gmail.com>
2023-10-29 03:34:15 +0300

    Drop extra noise
    I deleted a lot of assets that existed from past visions of the project

--- skriptos.js
+++ skriptos.js
@@ -2,5 +2,5 @@ function add() {
     var a = document.getElementById('a').value;
     var b = document.getElementById('b').value;
     var sum = Number(a) + Number(b);
-    alert("ThNe sum of the 2 numbers is" + sum);
+    alert("ThNe sum of the 2 numbers is " + sum);
 }
