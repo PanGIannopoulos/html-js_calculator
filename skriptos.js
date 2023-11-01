cb37dac23d0eb9d5abbd6d1b3860581a1701a3ad
Luminous <Panabougian@gmail.com>
2023-11-02 00:43:57 +0200

    add css styling
    added some colour and centered the app

--- skriptos.js
+++ skriptos.js
@@ -2,5 +2,30 @@ function add() {
     var a = document.getElementById('a').value;
     var b = document.getElementById('b').value;
     var sum = Number(a) + Number(b);
-    alert("ThNe sum of the 2 numbers is " + sum);
+    alert("The sum of the 2 numbers is " + sum);
+}
+
+function minus() {
+    var a = document.getElementById('a').value;
+    var b = document.getElementById('b').value;
+    var diff = Number(a) - Number(b);
+    alert("The difference of the 2 numbers is " + diff );
+}
+
+function multiply() {
+    var a = document.getElementById('a').value;
+    var b = document.getElementById('b').value;
+    var mult = Number(a) * Number(b);
+    alert("The multiplication of the 2 numbers is " + mult );
+}
+
+function divide() {
+    var a = document.getElementById('a').value;
+    var b = document.getElementById('b').value;
+    if (Number(b) == 0){
+        alert("division with 0 not possible");
+        return ;
+    }
+    var division = Number(a) / Number(b);
+    alert("The division of the 2 numbers is " + division );
 }
